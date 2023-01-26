e35697dc5d7bac34aa72d7fe26b72e0d0206730f
Denilson <denilsonsobreira0@gmail.com>
2023-01-26 14:13:53 -0300

    chore: add esl lint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    project: './tsconfig.json'
+  }
+}
+// {
+//     "extends": "standard-with-typescript",
+//     "parserOptions": {
+//         "project": "./tsconfig.json"
+//     }
+// }
