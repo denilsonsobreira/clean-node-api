65e7b8301ce786df20d9023d9589a727aa5c34eb
Denilson <denilsonsobreira0@gmail.com>
2023-03-29 21:58:55 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/cryptography/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { type AddAccountRepository } from '../../../../../data/protocols/add-account-repository'
+import { type AccountModel } from '../../../../../domain/models/account'
+import { type AddAccountModel } from '../../../../../domain/use-cases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const accountId = result.insertedId
+    const account = await accountCollection.findOne<AccountModel>({ _id: accountId }, {})
+    return Object.assign({}, account, { id: accountId.toString })
+  }
+}
